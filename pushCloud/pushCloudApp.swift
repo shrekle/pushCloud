d8dfa10b19f774ef90f77e763f6345cdd094a31a
shrekle <89948351+shrekle@users.noreply.github.com>
2023-05-26 07:47:55 -0800

    first init dawg

---
+++ pushCloud/pushCloudApp.swift
@@ -0,0 +1,17 @@
+//
+//  pushCloudApp.swift
+//  pushCloud
+//
+//  Created by nan on 5/26/23.
+//
+
+import SwiftUI
+
+@main
+struct pushCloudApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
